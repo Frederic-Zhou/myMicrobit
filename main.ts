99db1c2565614bffc2c19e2e5f304767adde8bf2
Frederic <zetachow@163.com>
2020-08-05 18:39:01 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,56 @@
-input.onButtonPressed(Button.AB, function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    randerPoint = randint(1, 6)
+    if (randerPoint == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (randerPoint == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+    } else if (randerPoint == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (randerPoint == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (randerPoint == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
 })
+let randerPoint = 0
 basic.showLeds(`
     . # # # .
     # # # # #
