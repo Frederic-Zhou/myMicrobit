187cb24067aac17da8b47d3376698c2c2df1bbe4
Frederic <zetachow@163.com>
2020-08-06 23:39:57 +0800

    添加扩展板功能

--- main.ts
+++ main.ts
@@ -1,56 +1,10 @@
-input.onGesture(Gesture.Shake, function () {
-    randerPoint = randint(1, 6)
-    if (randerPoint == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else if (randerPoint == 2) {
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . . . . .
-            . . # . .
-            . . . . .
-            `)
-    } else if (randerPoint == 3) {
-        basic.showLeds(`
-            . . . . .
-            . # . . .
-            . . # . .
-            . . . # .
-            . . . . .
-            `)
-    } else if (randerPoint == 4) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # . # .
-            . . . . .
-            `)
-    } else if (randerPoint == 5) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . # . .
-            . # . # .
-            . . . . .
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . # . # .
-            . # . # .
-            . . . . .
-            `)
-    }
-})
-let randerPoint = 0
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
 basic.showLeds(`
     . # # # .
     # # # # #
@@ -58,6 +12,7 @@ basic.showLeds(`
     . # # # .
     . # . # .
     `)
+SuperBit.Music(SuperBit.enMusic.dadadum)
 basic.forever(function () {
 	
 })
