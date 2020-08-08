86050d70a5d0795da47ce830a9aaab9a248fb995
Frederic <zetachow@163.com>
2020-08-08 23:37:24 +0800

    电机

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
 })
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    SuperBit.MotorStopAll()
+})
 input.onButtonPressed(Button.B, function () {
-    SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+    SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
 })
 basic.showLeds(`
     # # # # #
